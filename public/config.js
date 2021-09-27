1c9cd1c0d91c5a0acc90dc659b8d8a7fad462aa6
李彬 <180505@nd.com>
2021-09-27 11:33:25 +0800

    chore:更新vault地址。

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.config = {
   "localhost": {
-    "vault_address": "0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07",
+    "vault_address": "0x21dF544947ba3E8b3c32561399E88B52Dc8b2823",
     "underlying_address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
     "apy_server": "http://192.168.254.27:3000"
   },
