6c0d8f4bae3c36a25edeb34a590f53edee7803b0
李彬 <180505@nd.com>
2021-10-08 18:48:15 +0800

    chore:配置地址调整。

--- public/config.js
+++ public/config.js
@@ -2,12 +2,12 @@ window.config = {
   "localhost": {
     "vault_address": "0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07",
     "underlying_address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
-    "apy_server": "http://192.168.254.27:3000"
+    "apy_server": "http://192.168.254.27/api"
   },
   "dev": {
     "vault_address": "0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07",
     "underlying_address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
-    "apy_server": ""
+    "apy_server": "http://192.168.254.27/api"
   },
   "mainnet": {
     "vault_address": "0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07",
