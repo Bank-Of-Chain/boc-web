2ab8b28f3201b69e8ca51b231d866bd502f3437f
林炜 <457239@nd.com>
2021-10-08 19:52:19 +0800

    feat:本地启动配置文件更新

--- public/config.js
+++ public/config.js
@@ -1,11 +1,11 @@
 window.config = {
   "localhost": {
-    "vault_address": "0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07",
+    "vault_address": "0x202CCe504e04bEd6fC0521238dDf04Bc9E8E15aB",
     "underlying_address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
-    "apy_server": "http://192.168.254.27/api"
+    "apy_server": "http://localhost:80"
   },
   "dev": {
-    "vault_address": "0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07",
+    "vault_address": "0x202CCe504e04bEd6fC0521238dDf04Bc9E8E15aB",
     "underlying_address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
     "apy_server": "http://192.168.254.27/api"
   },
