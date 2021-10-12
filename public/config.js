af4388158f3d474251152aea0fbd59e1b3051a0b
李彬 <180505@nd.com>
2021-10-12 11:21:02 +0800

    chore: 配置文件端口调整。

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@ window.config = {
   "localhost": {
     "vault_address": "0x202CCe504e04bEd6fC0521238dDf04Bc9E8E15aB",
     "underlying_address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
-    "apy_server": "http://localhost:80"
+    "apy_server": "http://localhost:3000"
   },
   "dev": {
     "vault_address": "0x202CCe504e04bEd6fC0521238dDf04Bc9E8E15aB",
