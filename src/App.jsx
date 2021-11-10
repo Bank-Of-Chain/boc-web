d96c464c713077942a9d65cbd6c67d8dda052f05
李彬 <180505@nd.com>
2021-11-10 15:43:16 +0800

    refactor: loading page effect

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,19 @@ import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
 import { JsonRpcProvider, Web3Provider } from "@ethersproject/providers";
 import Web3Modal from "web3modal";
 import { useUserAddress } from "eth-hooks";
+
+// === Components === //
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Backdrop from '@material-ui/core/Backdrop';
+
+// === Utils === //
 import { useUserProvider, useGasPrice, useBalance } from "./hooks";
 import { RPC_URL } from "./constants";
 import { Transactor } from "./helpers";
 import isEmpty from 'lodash/isEmpty';
+import { makeStyles } from '@material-ui/core/styles';
+
+// === Styles === //
 import "./App.css";
 
 Date.prototype.format = function (fmt) {
@@ -51,7 +60,19 @@ const web3Modal = new Web3Modal({
   },
 });
 
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#BBB',
+    backgroundColor: '#e1e1e1'
+  },
+}));
+
 function App() {
+
+  const classes = useStyles();
+  console.log('classes=', classes)
   const [injectedProvider, setInjectedProvider] = useState();
 
   const loadWeb3Modal = useCallback(async () => {
@@ -96,12 +117,16 @@ function App() {
       <HashRouter>
         <Switch>
           <Route exact path="/">
-            <Suspense fallback={<div>Loading</div>}>
+            <Suspense fallback={<Backdrop className={classes.backdrop} open>
+              <CircularProgress color="inherit" />
+            </Backdrop>}>
               <Home {...nextProps} />
             </Suspense>
           </Route>
           <Route path="/invest">
-            <Suspense fallback={<div>Loading</div>}>
+            <Suspense fallback={<Backdrop className={classes.backdrop} open>
+              <CircularProgress color="inherit" />
+            </Backdrop>}>
               <Invest {...nextProps} />
             </Suspense>
           </Route>
