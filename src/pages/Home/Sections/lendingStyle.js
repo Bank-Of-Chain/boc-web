1dd3411eace69b9dfa95dc88b0076ee230cafee8
李彬 <180505@nd.com>
2022-01-24 18:02:34 +0800

    feateure: compoent add

---
+++ src/pages/Home/Sections/lendingStyle.js
@@ -0,0 +1,11 @@
+const lendingSectionStyle = {
+  section: {
+    padding: "70px 0",
+    textAlign: "center",
+  },
+  text: {
+    color: "#1cd9ff"
+  },
+};
+
+export default lendingSectionStyle;
