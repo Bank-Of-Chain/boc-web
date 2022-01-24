344383980054bf2b8e04ebdc67055df424fb4e60
李彬 <180505@nd.com>
2022-01-24 17:56:01 +0800

    chore: constants add

---
+++ src/constants/chains.js
@@ -0,0 +1,20 @@
+const ETH = {
+  id: '1',
+  name: 'ETH',
+}
+const BSC = {
+  id: '56',
+  name: 'BSC',
+}
+const MATIC = {
+  id: '137',
+  name: 'MATIC',
+}
+const CHAINS = [ETH, BSC, MATIC]
+
+export {
+  ETH,
+  BSC,
+  MATIC
+}
+export default CHAINS
