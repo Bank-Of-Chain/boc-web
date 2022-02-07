da7a137f5b3607e97bf53eb0d6a7e62357c3a0f6
李彬 <180505@nd.com>
2022-02-07 18:17:16 +0800

    feature: component add

---
+++ src/components/Stepper/StepConnector.jsx
@@ -0,0 +1,27 @@
+import { withStyles } from "@material-ui/core/styles"
+import StepConnector from "@material-ui/core/StepConnector"
+
+const BocStepConnector = withStyles({
+  alternativeLabel: {
+    top: 10,
+    left: "calc(-50% + 16px)",
+    right: "calc(50% + 16px)",
+  },
+  active: {
+    "& $line": {
+      borderColor: "#784af4",
+    },
+  },
+  completed: {
+    "& $line": {
+      borderColor: "#784af4",
+    },
+  },
+  line: {
+    borderColor: "#eaeaf0",
+    borderTopWidth: 3,
+    borderRadius: 1,
+  },
+})(StepConnector)
+
+export default BocStepConnector
---
+++ src/components/Stepper/StepLabel.jsx
@@ -0,0 +1,16 @@
+import { withStyles } from "@material-ui/core/styles"
+import StepLabel from "@material-ui/core/StepLabel"
+
+const BocStepLabel = withStyles({
+  label: {
+    color: "#fff",
+  },
+  active: {
+    color: "rgba(255, 255, 255, 0.87) !important",
+  },
+  completed: {
+    color: "rgba(255, 255, 255, 0.87) !important",
+  },
+})(StepLabel)
+
+export default BocStepLabel
