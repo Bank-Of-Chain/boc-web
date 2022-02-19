eb0e9e378789f5f8211d0841ca5f99074b55bd42
李彬 <180505@nd.com>
2022-02-19 11:48:45 +0800

    feature: add loading logic

--- src/pages/Home/Sections/LendingSection.jsx
+++ src/pages/Home/Sections/LendingSection.jsx
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles"
 // === Components === //
 import GridContainer from "../../../components/Grid/GridContainer"
 import GridItem from "../../../components/Grid/GridItem"
+import CircularProgress from "@material-ui/core/CircularProgress"
 
 // === Utils === //
 import map from "lodash/map"
@@ -27,9 +28,11 @@ const bocTitle = "BOC"
 const array = ["Compound", "Aave", "Coinbase", "BlockFi", "Nexo", "Celsius", "YearnFinance", "CryptoCom", "Bitfinex"]
 export default function LendingSection () {
   const classes = useStyles()
+  const [loading, setLoading] = useState(false)
   const [data, setData] = useState([])
 
   useEffect(() => {
+    setLoading(true)
     Promise.all([
       getETHLast30DaysVaultData().then(a => {
         return {
@@ -59,6 +62,11 @@ export default function LendingSection () {
         ]
       })
       .then(setData)
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false)
+        }, 1000)
+      })
   }, [])
   const maxPercentItem = maxBy(data, "percent")
   const displayMaxValue = 10 * Math.ceil(maxPercentItem?.percent / 10)
@@ -68,48 +76,56 @@ export default function LendingSection () {
         Trailing <span className={classes.text}>30-day</span> Crypto Average Lending Interest Rates
       </h2>
       <div style={{ padding: "4.5rem 0" }}>
-        <GridContainer style={{ margin: "0 auto" }} justify='center'>
-          {map(
-            filter(
-              sortBy(data, o => {
-                if (o.title === bocTitle) {
-                  return -10000
-                }
-                return -1 * o.percent
-              }),
-              o => !isNaN(o.percent),
-            ),
-            (item, i) => {
-              const { title, imagePath, percent } = item
-              const nextPercent = percent / displayMaxValue
-              const percentText = `${toFixed(nextPercent.toString(), 1e-2, 2)}%`
-              return (
-                <GridItem className={classNames(classes.item)} key={`${i}`} xs={3} sm={3} md={1}>
-                  <GridContainer className={classes.body}>
-                    <GridItem className={classes.header} style={i === 0 ? { borderLeft: 0 } : {}}>
-                      <div
-                        className={classNames(classes.bar, title === bocTitle && classes.checked)}
-                        style={{ height: percentText }}
-                      >
-                        <p
-                          style={
-                            title === bocTitle ? { width: "98px", left: "-50px", textAlign: "right", top: "-65px" } : {}
-                          }
+        {loading ? (
+          <GridContainer style={{ margin: "0 auto" }} justify='center'>
+            <CircularProgress />
+          </GridContainer>
+        ) : (
+          <GridContainer style={{ margin: "0 auto" }} justify='center'>
+            {map(
+              filter(
+                sortBy(data, o => {
+                  if (o.title === bocTitle) {
+                    return -10000
+                  }
+                  return -1 * o.percent
+                }),
+                o => !isNaN(o.percent),
+              ),
+              (item, i) => {
+                const { title, imagePath, percent } = item
+                const nextPercent = percent / displayMaxValue
+                const percentText = `${toFixed(nextPercent.toString(), 1e-2, 2)}%`
+                return (
+                  <GridItem className={classNames(classes.item)} key={`${i}`} xs={3} sm={3} md={1}>
+                    <GridContainer className={classes.body}>
+                      <GridItem className={classes.header} style={i === 0 ? { borderLeft: 0 } : {}}>
+                        <div
+                          className={classNames(classes.bar, title === bocTitle && classes.checked)}
+                          style={{ height: percentText }}
                         >
-                          {percent}% {title === bocTitle && <span>(From Feb. 8)</span>}
-                        </p>
-                      </div>
-                    </GridItem>
-                    <GridItem className={classes.footer}>
-                      <img title={title} src={imagePath} alt={title} />
-                      <p>{title}</p>
-                    </GridItem>
-                  </GridContainer>
-                </GridItem>
-              )
-            },
-          )}
-        </GridContainer>
+                          <p
+                            style={
+                              title === bocTitle
+                                ? { width: "98px", left: "-50px", textAlign: "right", top: "-65px" }
+                                : {}
+                            }
+                          >
+                            {percent}% {title === bocTitle && <span>(From Feb. 8)</span>}
+                          </p>
+                        </div>
+                      </GridItem>
+                      <GridItem className={classes.footer}>
+                        <img title={title} src={imagePath} alt={title} />
+                        <p>{title}</p>
+                      </GridItem>
+                    </GridContainer>
+                  </GridItem>
+                )
+              },
+            )}
+          </GridContainer>
+        )}
       </div>
     </div>
   )
