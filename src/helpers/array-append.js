592d56ac7de053bc1b5fa90d781120765161dba4
吴政衍 <160201@nd.com>
2022-04-02 10:28:14 +0800

    feat: apy last day rule update

--- src/helpers/array-append.js
+++ src/helpers/array-append.js
@@ -16,6 +16,11 @@ export const arrayAppendOfDay = (array = [], size, key = 'id', valueKey = 'value
       return firstSecond <= 1 * item[key] && 1 * item[key] < firstSecondNextDay;
     });
 
+    // 最新的一天没数据不显示
+    if (i === 0 && isEmpty(todayItem)) {
+      continue
+    }
+
     if (isEmpty(todayItem)) {
       rs.push({
         [key]: firstSecond,
