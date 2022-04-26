c0c21bed763bf3f5b19129171811b3db51983562
李彬 <180505@nd.com>
2022-04-26 21:01:32 +0800

    refactor: remove unused files

--- src/store.js
+++ src/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import metaReducer from './reducers/meta-reducer'
-import vaultReducer from './reducers/vault-reducer'
 
 export default configureStore({
   reducer: {
-    metaReducer,
-    vaultReducer
+    metaReducer
   },
   // redux中的数据尽量都需要可序列化，建议不存结构很复杂的对象
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
