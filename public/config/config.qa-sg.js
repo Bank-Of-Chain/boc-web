02abccca3e53f29d4a8b33c47cb5d2be6bf8f682
李彬 <180505@nd.com>
2022-04-27 14:34:06 +0800

    chore: config update

--- public/config/config.qa-sg.js
+++ public/config/config.qa-sg.js
@@ -14,21 +14,21 @@
   blog_url: 'https://piggyfinance.github.io/docs/zh/blog/',
   licenses_url: 'https://piggyfinance.github.io/docs/zh/licenses/',
   document_url: "https://docs.bankofchain.io",
-  boc_server: 'http://13.215.137.222/server',
+  boc_server: 'http://service-qa-sg.bankofchain.io',
   sub_graph_url: {
     '1': 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-eth',
     '56': 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-bsc',
     '137': 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-matic'
   },
   multiple_of_gas: 2,
-  dashboard_url: 'http://13.215.137.222/dashboard',
+  dashboard_url: 'http://dashboard-qa-sg.bankofchain.io',
   oracle_additional_slippage: 20
 }
 
 const config137 = {
   ...configBase,
   abi_version: 'beta-v1.5',
-  apy_server: 'http://13.215.137.222/api/137',
+  apy_server: 'http://qa-sg-keeper-polygon.bankofchain.io',
   vault_address: "0x204d2e5c581506e939295daf99079b590ace906e",
   usdt_address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
   usdc_address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
@@ -51,7 +51,7 @@ const config137 = {
 
 const config56 = {
   ...configBase,
-  apy_server: 'http://13.215.137.222/api/56',
+  apy_server: 'http://qa-sg-keeper-bsc.bankofchain.io',
   vault_address: "0x2C328D592819524F741A88A18572372CCE196782",
   usdt_address: "0x55d398326f99059fF775485246999027B3197955",
   usdc_address: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
@@ -73,7 +73,7 @@ const config56 = {
 
 const config1 = {
   ...configBase,
-  apy_server: 'http://13.215.137.222/api/1',
+  apy_server: 'qa-sg-keeper-eth.bankofchain.io',
   vault_address: "0x5302E909d1e93e30F05B5D6Eea766363D14F9892",
   usdt_address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
   usdc_address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
