e713cfd2017cf7bee89ec877ed64c122c5fcac82
吴政衍 <160201@nd.com>
2022-05-18 15:22:10 +0800

    feat: rpc use metamask

--- public/config/config.pr-sg.js
+++ public/config/config.pr-sg.js
@@ -20,9 +20,9 @@
     '137': 'https://api.thegraph.com/subgraphs/name/bankofchain/boc-subgraph-matic'
   },
   rpc_url: {
-    '1': "https://rpc.ankr.com/eth",
-    '56': "https://bsc-dataseed.binance.org/",
-    '137': "https://rpc-mainnet.maticvigil.com"
+    '1': "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+    '56': "https://bsc-dataseed.binance.org",
+    '137': "https://polygon-rpc.com"
   },
   multiple_of_gas: 1,
   dashboard_url: 'https://dashboard-v1.bankofchain.io',
