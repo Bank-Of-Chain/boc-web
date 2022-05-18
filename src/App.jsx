34a6d5d6772c880d1697164d5dd7f970130053ce
李彬 <180505@nd.com>
2022-05-18 17:36:06 +0800

    refactor: set router /invest disabled

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ Date.prototype.format = function (fmt) {
 }
 
 const Home = lazy(() => import("./pages/Home/index"))
-const Invest = lazy(() => import("./pages/Invest/index"))
+// const Invest = lazy(() => import("./pages/Invest/index"))
 const InvestNew = lazy(() => import("./pages/InvestNew/index"))
 const Ethi = lazy(() => import("./pages/Ethi/index"))
 
@@ -323,7 +323,7 @@ function App () {
               </Frame>
             </Suspense>
           </Route>
-          <Route path='/invest'>
+          {/* <Route path='/invest'>
             <Suspense
               fallback={
                 <Backdrop className={classes.backdrop} open>
@@ -335,7 +335,7 @@ function App () {
                 <Invest {...nextProps} />
               </Frame>
             </Suspense>
-          </Route>
+          </Route> */}
           <Route path='/mutils'>
             <Suspense
               fallback={
