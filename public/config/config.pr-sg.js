754ead16179810913e2cc389e9854043cb7849be
李彬 <180505@nd.com>
2022-06-02 16:59:49 +0800

    chore: config update

--- public/config/config.pr-sg.js
+++ public/config/config.pr-sg.js
@@ -25,7 +25,7 @@
     '137': "https://polygon-rpc.com"
   },
   multiple_of_gas: 1,
-  dashboard_url: 'https://dashboard-v1.bankofchain.io',
+  dashboard_url: 'https://dashboard.bankofchain.io',
   oracle_additional_slippage: 20,
   legacys: {}
 }
