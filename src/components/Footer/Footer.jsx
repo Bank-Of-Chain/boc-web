076768219809294218dbce8e15aeaac46c13785a
李彬 <180505@nd.com>
2022-06-09 14:39:21 +0800

    refactor: header and footer css update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,99 +1,105 @@
 /*eslint-disable*/
-import React from "react";
+import React from "react"
 // nodejs library to set properties for components
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from "classnames"
 // material-ui core components
-import { List, ListItem } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { List, ListItem } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+
+// === Components === //
+import GridContainer from "../../components/Grid/GridContainer"
+import GridItem from "../../components/Grid/GridItem"
 
 // === Constants === //
-import { COMMUNITY_URL, TELEGRAM_URL } from "./../../constants";
+import { COMMUNITY_URL, TELEGRAM_URL } from "./../../constants"
 
-import styles from "./footerStyle.js";
+import styles from "./footerStyle.js"
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(styles)
 
-export default function Footer(props) {
-  const classes = useStyles();
-  const { whiteFont } = props;
+export default function Footer (props) {
+  const classes = useStyles()
+  const { whiteFont } = props
   const footerClasses = classNames({
     [classes.footer]: true,
     [classes.footerWhiteFont]: whiteFont,
-  });
+  })
   const aClasses = classNames({
     [classes.a]: true,
     [classes.footerWhiteFont]: whiteFont,
-  });
+  })
   return (
     <footer className={footerClasses}>
       <div className={classes.container}>
-        <div className={classes.center}>
-          This project is in beta. Use at your own risk.
-        </div>
-        <div className={classes.center}>
-          <List className={classes.list}>
-            <ListItem className={classes.inlineBlock}>
-              <a
-                className={classes.block}
-                target="_blank"
-                href={COMMUNITY_URL}
-              >
-                Discord
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a
-                className={classes.block}
-                target="_blank"
-                href={TELEGRAM_URL}
-              >
-                TELEGRAM
-              </a>
-            </ListItem>
-            {/* <ListItem className={classes.inlineBlock}>
-              <a
-                className={classes.block}
-                target="_blank"
-                href={ABOUTUS_URL}
-              >
-                About us
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a
-                className={classes.block}
-                target="_blank"
-                href={BLOG_URL}
-              >
-                Blog
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a
-                className={classes.block}
-                target="_blank"
-                href={LICENSES_URL}
-              >
-                Licenses
-              </a>
-            </ListItem> */}
-            <ListItem className={classes.inlineBlock}>
-              <a
-                className={classes.block}
-                target="_blank"
-              >
-                Copyright@{1900 + new Date().getYear()}
-              </a>
-            </ListItem>
-          </List>
-        </div>
+        <GridContainer>
+          <GridItem xs={6} sm={6} md={6}>
+            This project is in beta. Use at your own risk.
+          </GridItem>
+          <GridItem xs={6} sm={6} md={6}>
+            <List className={classes.list}>
+              <ListItem className={classes.inlineBlock}>
+                <a className={classes.block} target='_blank' href={COMMUNITY_URL}>
+                  Discord
+                </a>
+              </ListItem>
+              <ListItem className={classes.inlineBlock}>
+                <a className={classes.block} target='_blank' href={TELEGRAM_URL}>
+                  TELEGRAM
+                </a>
+              </ListItem>
+              <ListItem className={classes.inlineBlock}>
+                <a className={classes.block} target='_blank'>
+                  Copyright@{1900 + new Date().getYear()}
+                </a>
+              </ListItem>
+            </List>
+          </GridItem>
+          <GridItem xs={6} sm={6} md={6}>
+            This project is in beta. Use at your own risk.
+          </GridItem>
+          <GridItem xs={6} sm={6} md={6}>
+            <List className={classes.list}>
+              <ListItem className={classes.inlineBlock}>
+                <a className={classes.block} target='_blank' href={COMMUNITY_URL}>
+                  Discord
+                </a>
+              </ListItem>
+              <ListItem className={classes.inlineBlock}>
+                <a className={classes.block} target='_blank' href={TELEGRAM_URL}>
+                  TELEGRAM
+                </a>
+              </ListItem>
+              <ListItem className={classes.inlineBlock}>
+                <a className={classes.block} target='_blank'>
+                  Copyright@{1900 + new Date().getYear()}
+                </a>
+              </ListItem>
+            </List>
+          </GridItem>
+
+          <GridItem xs={12} sm={12} md={12}>
+            <List className={classes.list}>
+              <ListItem className={classes.inlineBlock}>
+                <a className={classes.block} target='_blank' href={COMMUNITY_URL}>
+                  Discord
+                </a>
+              </ListItem>
+              <ListItem className={classes.inlineBlock}>
+                <a className={classes.block} target='_blank' href={TELEGRAM_URL}>
+                  TELEGRAM
+                </a>
+              </ListItem>
+              <ListItem className={classes.inlineBlock}>
+                <a className={classes.block} target='_blank'>
+                  Copyright@{1900 + new Date().getYear()}
+                </a>
+              </ListItem>
+            </List>
+          </GridItem>
+        </GridContainer>
       </div>
     </footer>
-  );
+  )
 }
-
-Footer.propTypes = {
-  whiteFont: PropTypes.bool,
-};
--- src/components/Footer/footerStyle.js
+++ src/components/Footer/footerStyle.js
@@ -1,8 +1,8 @@
-import { container, primaryColor } from "../../assets/jss/material-kit-react.js";
+import { container, primaryColor } from "../../assets/jss/material-kit-react.js"
 
 const footerStyle = {
   block: {
-    color: '#707eac',
+    color: "#707eac",
     padding: "0.9375rem",
     fontWeight: "500",
     fontSize: "12px",
@@ -22,14 +22,14 @@ const footerStyle = {
     float: "right!important",
   },
   footer: {
-    height: '160px',
-    backgroundColor: '#06001e',
+    height: "160px",
+    backgroundColor: "#06001e",
     padding: "0.9375rem 0",
     textAlign: "center",
     display: "flex",
     zIndex: "2",
     position: "relative",
-    cursor: "pointer"
+    cursor: "pointer",
   },
   a: {
     color: primaryColor,
@@ -41,10 +41,7 @@ const footerStyle = {
       color: "#FFFFFF",
     },
   },
-  container:{
-    ...container,
-    margin: '40px 0px'
-  },
+  container: container,
   list: {
     marginBottom: "0",
     padding: "0",
@@ -61,10 +58,10 @@ const footerStyle = {
     position: "relative",
     top: "3px",
   },
-  center:{
-    color: '#707eac',
-    fontSize: '14px',
-    padding: '15px'
-  }
-};
-export default footerStyle;
+  center: {
+    color: "#707eac",
+    fontSize: "14px",
+    padding: "15px",
+  },
+}
+export default footerStyle
