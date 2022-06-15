bbcb2e8e85c684bc6db037c899ecbe2ae655203c
吴政衍 <160201@nd.com>
2022-06-15 14:08:06 +0800

    feat: update roadmap text

--- src/pages/Home/Sections/RoadMapSection.jsx
+++ src/pages/Home/Sections/RoadMapSection.jsx
@@ -10,19 +10,6 @@ const useStyles = makeStyles(styles)
 export default function RoadMapSection () {
   const classes = useStyles()
   const stepOne = [{
-    date: 'May 15, 2022',
-    event: 'BOC 2 Version'
-  }, {
-    date: 'May 10, 2022',
-    event: 'Security audit'
-  }, {
-    date: 'July 15, 2022',
-    event: 'Leveraging services'
-  }, {
-    date: 'August 1, 2022',
-    event: 'BOC 1.5 Version'
-  }]
-  const stepTwo = [{
     date: 'March 4, 2022',
     event: 'Friend & Family Testing'
   }, {
@@ -38,15 +25,28 @@ export default function RoadMapSection () {
     date: 'April 15, 2022',
     event: 'AWS server leasing and deployment'
   }]
+  const stepTwo = [{
+    date: 'May 10, 2022',
+    event: 'BOC 1.5 Version'
+  }, {
+    date: 'May 15, 2022',
+    event: 'Security audit'
+  }, {
+    date: 'July 15, 2022',
+    event: 'Leveraging services'
+  }, {
+    date: 'August 1, 2022',
+    event: 'BOC 2 Version'
+  }]
   return (
     <div className={classes.roadmap}>
       <h3 className={classes.title}>Roadmap.</h3>
       <Timeline
-        events={stepTwo}
+        events={stepOne}
         topText="NOW"
       />
       <Timeline
-        events={stepOne}
+        events={stepTwo}
         topText="NEXT STEPS"
         firstPlace="left"
         cardColorReverse
