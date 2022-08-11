a271c2d95276c8f8c3438985f66e6fdb43a06219
韩旭 <js_bin@163.com>
2022-08-11 19:05:23 +0800

    tests: testcases add

---
+++ src/__tests__/components/CustomButtons/CustomButtons.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import CustomButtons from "../../../components/CustomButtons/Button";
+
+test("CustomButtons Component Render", () => {
+  const { asFragment } = render(<CustomButtons className="abc" />);
+  expect(asFragment()).toMatchSnapshot();
+});
---
+++ src/__tests__/components/CustomDropdown/CustomDropdown.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import CustomDropdown from "../../../components/CustomDropdown/CustomDropdown";
+
+test("CustomDropdown Component Render", () => {
+  const { asFragment } = render(
+    <CustomDropdown
+      hoverColor="primary"
+      buttonProps={{ className: "abc" }}
+      buttonText="abc"
+      buttonIcon="abc"
+    />
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
