0554b694dbdf388bfc3c95e74a2790fc1e7c56bc
韩旭 <js_bin@163.com>
2022-08-12 11:11:04 +0800

    refactor: file rename

--- src/__tests__/components/TestComponent/TestComponent.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import TestComponent from "../../../components/TestComponent/TestComponent";
-
-test("TestComponent Component Render", () => {
-  const { queryByText, asFragment } = render(<TestComponent />);
-  expect(queryByText("TestComponent")).toBeTruthy();
-  expect(asFragment()).toMatchSnapshot();
-});
--- src/hooks/useLookupAddress.js
+++ src/hooks/useLookupAddress.js
@@ -1,27 +1,9 @@
 import { useState, useEffect } from "react";
 import { getAddress } from "@ethersproject/address";
-import useLocalStorage from "./LocalStorage";
-
-// resolved if(name){} to not save "" into cache
-
-/*
-  ~ What it does? ~
-
-  Gets ENS name from given address and provider
-
-  ~ How can I use? ~
-
-  const ensName = useLookupAddress(mainnetProvider, address);
-
-  ~ Features ~
-
-  - Provide address and get ENS name corresponding to given address
-*/
+import useLocalStorage from "./useLocalStorage";
 
 const lookupAddress = async (provider, address) => {
   try {
-    // Accuracy of reverse resolution is not enforced.
-    // We then manually ensure that the reported ens name resolves to address
     const reportedName = await provider.lookupAddress(address);
 
     const resolvedAddress = await provider.resolveName(reportedName);
