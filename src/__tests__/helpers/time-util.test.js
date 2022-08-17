2af66c6b17fb3dbd15707a8a4b0b9b76ab68b218
韩旭 <js_bin@163.com>
2022-08-17 15:22:41 +0800

    tests:  testcase update

--- src/__tests__/helpers/time-util.test.js
+++ src/__tests__/helpers/time-util.test.js
@@ -6,14 +6,21 @@ import moment from 'moment'
 test('getLastAllocationTime return correct time', () => {
   const text = getLastAllocationTime()
   // next Monday 7:00am at beijing timezone
-  const nextAllocationTime = moment().startOf('week').subtract(-8, 'day').set('hour', 7).set('minute', 0).set('second', 0).set('millisecond', 0)
+  const nextAllocationTime = moment()
+    .utcOffset(8)
+    .startOf('week')
+    .subtract(-8, 'day')
+    .set('hour', 7)
+    .set('minute', 0)
+    .set('second', 0)
+    .set('millisecond', 0)
   expect(text).toBe(nextAllocationTime.valueOf())
 })
 
 test('getLastDohardworkTime return correct time', () => {
   const text = getLastDohardworkTime()
   // next day 7:00am at beijing timezone
-  const nextDohardworkTime = moment().subtract(-1, 'day').set('hour', 7).set('minute', 0).set('second', 0).set('millisecond', 0)
+  const nextDohardworkTime = moment().utcOffset(8).subtract(-1, 'day').set('hour', 7).set('minute', 0).set('second', 0).set('millisecond', 0)
   expect(text).toBe(nextDohardworkTime.valueOf())
 })
 
