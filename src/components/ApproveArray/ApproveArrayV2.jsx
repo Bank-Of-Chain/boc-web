b64a4fc604f195a47755f729462e605a4db61c93
jiangcx <786255@nd.com>
2022-09-27 15:34:56 +0800

    feat: exchangePlatformAdapters

--- src/components/ApproveArray/ApproveArrayV2.jsx
+++ src/components/ApproveArray/ApproveArrayV2.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect, useState, useCallback, useRef } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import classNames from 'classnames'
 
@@ -82,6 +82,10 @@ const ApproveArrayV2 = props => {
     onSlippageChange
   } = props
 
+  const refArray = map(tokens, () => useRef(null))
+  console.groupCollapsed('init state')
+  console.log('refArray=', refArray)
+  console.groupEnd('init state')
   const initValues = map(tokens, () => undefined)
   const initBoolValues = map(tokens, () => false)
   const initNumberValues = map(tokens, () => 0)
@@ -89,6 +93,7 @@ const ApproveArrayV2 = props => {
     return {}
   })
 
+  const [exchangePlatformAdapters, setExchangePlatformAdapters] = useState({})
   const [receiveToken, setReceiveToken] = useState(isEthi ? ETH_ADDRESS : USDT_ADDRESS)
   // input values
   const [values, setValues] = useState([])
@@ -215,12 +220,12 @@ const ApproveArrayV2 = props => {
 
   const getExchangePlatformAdapters = async (exchangeAggregator, userProvider) => {
     const { _exchangeAdapters: adapters } = await exchangeAggregator.getExchangeAdapters()
-    const exchangePlatformAdapters = {}
+    const exchangeAdapters = {}
     for (const address of adapters) {
       const contract = new ethers.Contract(address, EXCHANGE_ADAPTER_ABI, userProvider)
-      exchangePlatformAdapters[await contract.identifier()] = address
+      exchangeAdapters[await contract.identifier()] = address
     }
-    return exchangePlatformAdapters
+    setExchangePlatformAdapters(exchangeAdapters)
   }
 
   const selectOptions = isEthi
@@ -256,172 +261,6 @@ const ApproveArrayV2 = props => {
   const receiveTokenDecimals = selectOptions.find(el => el.value === receiveToken).decimal
   const receiveTokenAmount = values[findIndex(tokens, el => el.address === receiveToken)] || '0'
 
-  // Get values, balances, decimals, allowances
-  const reload = useCallback(async () => {
-    console.log('---reload---')
-    if (isEmpty(tokens) || isEmpty(exchangeManager) || isEmpty(userAddress)) {
-      return
-    }
-    setIsReload(true)
-    const array = await Promise.all(
-      map(tokens, async token => {
-        const { address, amount } = token
-        if (address === ETH_ADDRESS) {
-          return {
-            address,
-            amount,
-            decimal: 18,
-            allowance: '0',
-            balance: (await userProvider.getBalance(userAddress).catch(() => BN_0)).toString()
-          }
-        }
-        const contract = new Contract(address, IERC20_ABI, userProvider)
-        const allowance = (await contract.allowance(userAddress, exchangeManager).catch(() => BN_0)).toString()
-        const balance = (await contract.balanceOf(userAddress).catch(() => BN_0)).toString()
-        const decimal = await contract.decimals().catch(() => BN_0)
-        return {
-          address,
-          amount,
-          decimal,
-          allowance,
-          balance
-        }
-      })
-    )
-    const { nextAllowances, nextBalances, nextDecimals, nextValues } = reduce(
-      array,
-      (rs, item, i) => {
-        const { allowance, balance, amount, decimal } = item
-        rs.nextAmounts[i] = amount
-        rs.nextBalances[i] = balance
-        rs.nextDecimals[i] = BigNumber.from(10).pow(decimal)
-        rs.nextAllowances[i] = allowance
-        rs.nextValues[i] = values[i] || toFixed(amount, BigNumber.from(10).pow(decimal))
-        return rs
-      },
-      {
-        nextAllowances: [],
-        nextBalances: [],
-        nextAmounts: [],
-        nextDecimals: [],
-        nextValues: []
-      }
-    )
-    setValues(nextValues)
-    setBalances(nextBalances)
-    setDecimals(nextDecimals)
-    setAllowances(nextAllowances)
-    setIsReload(false)
-  }, [tokens, userAddress, values, exchangeManager])
-
-  const handleInputChange = (val, index) => {
-    const num = Number(val)
-    // Maybe num is NaN
-    if (!(num >= 0)) {
-      return
-    }
-    const nextValues = map(tokens, (v, i) => {
-      if (i === index) return val
-      return values[i]
-    })
-    setValues(nextValues)
-    debounceUpdate(num, index, nextValues)
-  }
-
-  const debounceUpdate = useCallback(
-    debounce((val, index, nextValues) => {
-      const nextSwapInfoArray = map(swapInfoArray, (item, i) => {
-        if (i === index) return undefined
-        return item
-      })
-      const nextDoneArray = map(doneArray, (item, i) => {
-        if (i === index) return false
-        return item
-      })
-      const nextIsSwapInfoFetching = map(isSwapInfoFetching, (item, i) => {
-        if (i === index) {
-          return !isErrorValue(index, val) && !isEmptyValue(index, val)
-        }
-        return item
-      })
-      const nextExcludeArray = map(excludeArray, (item, i) => {
-        if (i === index) return {}
-        return item
-      })
-      setDoneArray(nextDoneArray)
-      setSwapInfoArray(nextSwapInfoArray)
-      setIsStaticCalling(initBoolValues)
-      setIsSwapInfoFetching(nextIsSwapInfoFetching)
-      setRetryTimesArray(initNumberValues)
-      setExcludeArray(nextExcludeArray)
-      estimateWithValue(nextValues, index)
-    }, 1500),
-    [swapInfoArray, doneArray, isSwapInfoFetching, excludeArray]
-  )
-
-  const approveValue = async index => {
-    console.log(`---approveValue ${index}---`)
-    const token = get(tokens, index)
-    const value = get(values, index)
-    const decimal = get(decimals, index)
-    // ETH no need approve
-    if (isEmpty(token) || isNil(value) || value === '0') return
-    const { address } = token
-    const signer = userProvider.getSigner()
-    const contract = new Contract(address, IERC20_ABI, userProvider)
-    const contractWithUser = contract.connect(signer)
-    let nextValue
-    try {
-      nextValue = BigNumber.from(new BN(value).multipliedBy(decimal.toString()).toFixed())
-    } catch (e) {
-      return
-    }
-    const allowanceAmount = await contractWithUser.allowance(userAddress, exchangeManager)
-    // If deposit amount greater than allow amount, reset amount
-    if (nextValue.gt(allowanceAmount)) {
-      // If allowance equal 0, approve nextAmount, otherwise increaseAllowance
-      if (allowanceAmount.gt(0)) {
-        if (address === WETH_ADDRESS) {
-          // WETH don't support increaseAllowance
-          return contractWithUser
-            .approve(exchangeManager, 0)
-            .then(tx => tx.wait())
-            .then(() => {
-              reload()
-              return contractWithUser.approve(exchangeManager, nextValue).then(tx => tx.wait())
-            })
-        }
-        return contractWithUser
-          .increaseAllowance(exchangeManager, nextValue.sub(allowanceAmount))
-          .then(tx => tx.wait())
-          .catch(e => {
-            // cancel by user
-            if (e.code === 4001) {
-              return Promise.reject(e)
-            }
-            // If increase failed, approve 0 and approve nextAmounts
-            return contractWithUser
-              .approve(exchangeManager, 0)
-              .then(tx => tx.wait())
-              .then(() => {
-                reload()
-                return contractWithUser.approve(exchangeManager, nextValue).then(tx => tx.wait())
-              })
-          })
-      } else {
-        return contractWithUser
-          .approve(exchangeManager, nextValue)
-          .then(tx => tx.wait())
-          .catch(e => {
-            // cancel by user
-            if (e.code === 4001) {
-              return Promise.reject(e)
-            }
-          })
-      }
-    }
-  }
-
   const approveAll = async () => {
     console.groupCollapsed('approveAll call')
     setIsSwapping(true)
@@ -444,37 +283,6 @@ const ApproveArrayV2 = props => {
       return
     }
     console.groupEnd('approveAll call')
-  }
-
-  // swap triggered by useEffect
-  const swap = async () => {
-    console.groupCollapsed('swap call')
-    console.log('isSwapping=', isSwapping)
-    if (!isSwapping) {
-      return
-    }
-    if (isEmpty(swapInfoArray)) {
-      setIsSwapping(false)
-      return
-    }
-    console.log('swapInfoArray=', swapInfoArray)
-    const someApproveNotEnough = some(tokens, (token, index) => {
-      return isApproveNotEnough(index)
-    })
-    console.log('someApproveNotEnough=', someApproveNotEnough)
-    if (someApproveNotEnough) {
-      dispatch(
-        warmDialog({
-          open: true,
-          type: 'warning',
-          message: 'Please approve the amount of coins to be exchanged in full!'
-        })
-      )
-      setIsSwapping(false)
-      return
-    }
-    realSwap()
-    console.groupEnd('swap call')
   }
 
   // All done, swap
@@ -543,182 +351,9 @@ const ApproveArrayV2 = props => {
     console.groupEnd('realSwap call')
   }
 
-  const queryBestSwapInfo = useCallback(
-    async (token, value, decimal, index, exchangePlatformAdapters) => {
-      console.groupCollapsed('queryBestSwapInfo call')
-      console.log('excludeArray=', excludeArray)
-      console.log('value=', value)
-      console.log('index=', index)
-      const swapInfoItem = swapInfoArray[index]
-      if (!isEmpty(swapInfoItem) && !(swapInfoItem instanceof Error)) return swapInfoItem
-      if (isNil(decimal) || isNil(value) || value === '0' || token.address === receiveToken) return
-      const nextFromValueString = new BN(value).multipliedBy(decimal.toString()).toFixed()
-      if (nextFromValueString.indexOf('.') !== -1) return
-      const swapAmount = BigNumber.from(nextFromValueString)
-      let fromToken = {}
-      let toToken = {}
-      if (token.address === ETH_ADDRESS) {
-        fromToken = {
-          decimals: 18,
-          address: token.address
-        }
-      } else {
-        const fromTokenConstract = new Contract(token.address, IERC20_ABI, userProvider)
-        fromToken = {
-          decimals: parseInt((await fromTokenConstract.decimals()).toString()),
-          address: token.address
-        }
-      }
-      if (receiveToken === ETH_ADDRESS) {
-        toToken = {
-          decimals: 18,
-          address: receiveToken
-        }
-      } else {
-        const toTokenConstract = new Contract(receiveToken, IERC20_ABI, userProvider)
-        toToken = {
-          decimals: parseInt((await toTokenConstract.decimals()).toString()),
-          address: receiveToken
-        }
-      }
-      console.groupCollapsed('fetch best swap path: ' + fromToken.address + '-' + toToken.address + '-' + index)
-      let nextExchangeExtraParams = assign({}, EXCHANGE_EXTRA_PARAMS, isEmpty(exchangePlatformAdapters.testAdapter) ? {} : {})
-
-      if (!isEmpty(excludeArray[index])) {
-        console.log(`excludeArray[${index}]`, excludeArray[index])
-        const { oneInchV4 = [], paraswap = [] } = excludeArray[index]
-        nextExchangeExtraParams = assign({}, nextExchangeExtraParams, {
-          oneInchV4: {
-            ...nextExchangeExtraParams.oneInchV4,
-            excludeProtocols: uniq(oneInchV4.concat(nextExchangeExtraParams.oneInchV4.excludeProtocols))
-          },
-          paraswap: {
-            ...nextExchangeExtraParams.paraswap,
-            excludeDEXS: uniq([...paraswap, nextExchangeExtraParams.paraswap.excludeDEXS.split(',')]).join(',')
-          }
-        })
-      }
-      const bestSwapInfo = await getBestSwapInfo(
-        fromToken,
-        toToken,
-        swapAmount,
-        parseInt(100 * parseFloat(slippage)) || 0,
-        ORACLE_ADDITIONAL_SLIPPAGE,
-        exchangePlatformAdapters,
-        nextExchangeExtraParams
-      ).catch(error => {
-        throw new Error(error)
-      })
-      console.groupEnd('fetch best swap path: ' + fromToken.address + '-' + toToken.address + '-' + index)
-      if (isEmpty(bestSwapInfo)) {
-        throw new Error('fetch error')
-      }
-      console.groupEnd('queryBestSwapInfo call')
-      return {
-        bestSwapInfo,
-        info: {
-          amount: swapAmount,
-          srcToken: token.address,
-          dstToken: receiveToken,
-          receiver: userAddress
-        }
-      }
-    },
-    [excludeArray, receiveToken, slippage]
-  )
-
-  const estimateWithValue = useCallback(
-    debounce(async (newValues, refreshIndex) => {
-      console.groupCollapsed('estimateWithValue call')
-      if (isEmpty(receiveToken)) return
-      console.log('receiveToken=', receiveToken)
-      console.log('swapInfoArray=', swapInfoArray)
-      console.log('newValues=', newValues)
-      const exchangeManagerContract = new Contract(exchangeManager, EXCHANGE_AGGREGATOR_ABI, userProvider)
-      const exchangePlatformAdapters = await getExchangePlatformAdapters(exchangeManagerContract, userProvider)
-      const nextIsSwapInfoFetching = map(swapInfoArray, (item, index) => {
-        if (isReciveToken(index) || isErrorValue(index) || isEmptyValue(index)) {
-          return false
-        }
-        if (retryTimesArray[index] > MAX_RETRY_TIME) {
-          setIsSwapping(false)
-          return false
-        }
-        if (!isNil(refreshIndex)) {
-          return refreshIndex === index
-        }
-        return isEmpty(item) || item instanceof Error
-      })
-      console.log('nextIsSwapInfoFetching=', nextIsSwapInfoFetching)
-      setIsSwapInfoFetching(nextIsSwapInfoFetching)
-      const requestArray = map(nextIsSwapInfoFetching, (item, index) => {
-        if (!item) return swapInfoArray[index]
-        return queryBestSwapInfo(tokens[index], newValues[index] || '0', decimals[index], index, exchangePlatformAdapters).catch(() => {
-          return
-        })
-      })
-      Promise.all(requestArray)
-        .then(nextSwapArray => {
-          if (!isEqual(nextSwapArray, swapInfoArray)) {
-            console.log('nextSwapArray', nextSwapArray)
-            setSwapInfoArray(nextSwapArray)
-          }
-          const nextRetryTimesArray = map(retryTimesArray, (item, i) => {
-            if (!isReciveToken(i) && !nextSwapArray[i] && !isErrorValue(i) && !isEmptyValue(i)) {
-              return min([item + 1, MAX_RETRY_TIME + 1])
-            }
-            return item
-          })
-          if (!isEqual(nextRetryTimesArray, retryTimesArray)) {
-            console.log('nextRetryTimesArray', nextRetryTimesArray)
-            setRetryTimesArray(nextRetryTimesArray)
-          }
-          const nextIsSwapInfoFetching = map(tokens, (item, index) => {
-            if (isReciveToken(index) || retryTimesArray[index] > MAX_RETRY_TIME || isErrorValue(index) || isEmptyValue(index)) {
-              return false
-            }
-            return !doneArray[index] && !isApproveNotEnough(index)
-          })
-          setIsSwapInfoFetching(nextIsSwapInfoFetching)
-        })
-        .catch(() => {
-          setIsSwapInfoFetching(initBoolValues)
-        })
-      console.groupEnd('estimateWithValue call')
-    }, 1500),
-    [receiveToken, exchangeManager, tokens, decimals, swapInfoArray, retryTimesArray, doneArray, queryBestSwapInfo]
-  )
-
-  const reloadSwap = index => {
-    console.groupCollapsed('reloadSwap call')
-    const nextSwapInfoArray = map(swapInfoArray, (item, i) => {
-      if (i === index) return undefined
-      return item
-    })
-
-    const nextIsSwapInfoFetching = map(isSwapInfoFetching, (item, i) => {
-      if (i === index) return true
-      return false
-    })
-
-    const nextRetryTimesArray = map(retryTimesArray, (item, i) => {
-      if (i === index) return 0
-      return item
-    })
-    console.log('nextSwapInfoArray=', nextSwapInfoArray)
-    console.log('nextIsSwapInfoFetching=', nextIsSwapInfoFetching)
-    console.log('nextRetryTimesArray=', nextRetryTimesArray)
-
-    setSwapInfoArray(nextSwapInfoArray)
-    setIsSwapInfoFetching(nextIsSwapInfoFetching)
-    setRetryTimesArray(nextRetryTimesArray)
-    estimateWithValue(values, index)
-    console.groupEnd('reloadSwap call')
-  }
-
   // approve the current token with current value
   const approve = index => {
-    return approveValue(index).then(reload)
+    return
   }
 
   const clickSwap = () => {
@@ -745,22 +380,19 @@ const ApproveArrayV2 = props => {
   }
 
   useEffect(() => {
-    if (isEmpty(userAddress)) return
-    reload()
-    // const timer = setInterval(reload, 10000)
-    // return () => clearInterval(timer)
-  }, [tokens, userAddress, exchangeManager])
-
-  const exchangePlatformAdapters = {}
+    const exchangeManagerContract = new Contract(exchangeManager, EXCHANGE_AGGREGATOR_ABI, userProvider)
+    getExchangePlatformAdapters(exchangeManagerContract, userProvider)
+  }, [exchangeManager])
 
   return (
     <div className={classes.main}>
       <div className={classes.title}>Swap to single token</div>
       <div className={classes.approveContainer}>
         <div>Swap tokens:</div>
-        {map(tokens, token => {
+        {map(tokens, (token, index) => {
           return (
             <TokenItem
+              // ref={refArray[index]}
               key={token.address}
               userAddress={userAddress}
               userProvider={userProvider}
