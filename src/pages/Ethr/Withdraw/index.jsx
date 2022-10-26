06faf78a8d53758232ad6cd01b138fae6dd4e61e
tianxiuali <627786255@qq.com>
2022-10-26 19:55:44 +0800

    fix: bignumber toFixed

--- src/pages/Ethr/Withdraw/index.jsx
+++ src/pages/Ethr/Withdraw/index.jsx
@@ -74,7 +74,7 @@ export default function Withdraw({
       const vaultContractWithSigner = vaultContract.connect(signer)
       setCurrentStep(2)
       let tx
-      const withdrawAmount = BigNumber.from(BN(toValue).times(BN(10).pow(wantTokenDecimals)).toString())
+      const withdrawAmount = BigNumber.from(BN(toValue).times(BN(10).pow(wantTokenDecimals)).toFixed())
       // if gasLimit times not 1, need estimateGas
       if (isNumber(MULTIPLE_OF_GAS) && MULTIPLE_OF_GAS !== 1) {
         const gas = await vaultContractWithSigner.estimateGas.redeem(withdrawAmount, estimatedTotalAssets)
