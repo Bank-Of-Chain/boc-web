5e8920c83bf57aebc0aedcb330bf8c00d4a733a9
韩旭gogogo <js_bin@163.com>
2022-11-16 11:42:37 +0800

    feature: icon upgrade

--- src/pages/Ethi/index.jsx
+++ src/pages/Ethi/index.jsx
@@ -10,10 +10,6 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import SwapHorizIcon from '@material-ui/icons/SwapHoriz'
-import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet'
-import SaveAltIcon from '@material-ui/icons/SaveAlt'
-import UndoIcon from '@material-ui/icons/Undo'
 import Tooltip from '@material-ui/core/Tooltip'
 import InfoIcon from '@material-ui/icons/Info'
 import Loading from '@/components/LoadingComponent'
@@ -23,7 +19,7 @@ import useMediaQuery from '@material-ui/core/useMediaQuery'
 import Deposit from './Deposit'
 import Withdraw from './Withdraw'
 import MyStatement from '@/components/MyStatement'
-import InsertChartIcon from '@material-ui/icons/InsertChart'
+import { MyAccountIcon, SwapIcon, WithdrawIcon, DepositIcon, SwitchIcon } from '@/components/SvgIcons'
 
 import { useSelector, useDispatch } from 'react-redux'
 
@@ -192,22 +188,12 @@ function Ethi(props) {
           <List>
             <ListItem key="My Account" button className={classNames(classes.item)} onClick={() => setCurrent(INVEST_TAB.account)}>
               <ListItemIcon>
-                <AccountBalanceWalletIcon style={{ color: current === INVEST_TAB.account ? '#A68EFE' : '#fff' }} />
+                <MyAccountIcon style={{ color: current === INVEST_TAB.account ? '#A68EFE' : '#fff' }} />
               </ListItemIcon>
               {!isLayoutSm && (
                 <ListItemText primary={'My Account'} className={classNames(current === INVEST_TAB.account ? classes.check : classes.text)} />
               )}
             </ListItem>
-            {!isEmpty(address) && (
-              <ListItem key="My Statement" button className={classNames(classes.item)} onClick={() => setCurrent(INVEST_TAB.statement)}>
-                <ListItemIcon>
-                  <InsertChartIcon style={{ color: current === INVEST_TAB.statement ? '#A68EFE' : '#fff' }} />
-                </ListItemIcon>
-                {!isLayoutSm && (
-                  <ListItemText primary={'My Statement'} className={classNames(current === INVEST_TAB.statement ? classes.check : classes.text)} />
-                )}
-              </ListItem>
-            )}
             <ListItem
               key="Deposit"
               button
@@ -215,7 +201,7 @@ function Ethi(props) {
               onClick={() => setCurrent(INVEST_TAB.deposit)}
             >
               <ListItemIcon>
-                <SaveAltIcon style={{ color: current === INVEST_TAB.deposit ? '#A68EFE' : '#fff' }} />
+                <DepositIcon style={{ color: current === INVEST_TAB.deposit ? '#A68EFE' : '#fff' }} />
               </ListItemIcon>
               {!isLayoutSm && (
                 <ListItemText primary={'Deposit'} className={classNames(current === INVEST_TAB.deposit ? classes.check : classes.text)} />
@@ -223,15 +209,26 @@ function Ethi(props) {
             </ListItem>
             <ListItem key="Withdraw" button className={classNames(classes.item)} onClick={() => setCurrent(INVEST_TAB.withdraw)}>
               <ListItemIcon>
-                <UndoIcon style={{ color: current === INVEST_TAB.withdraw ? '#A68EFE' : '#fff' }} />
+                <WithdrawIcon style={{ color: current === INVEST_TAB.withdraw ? '#A68EFE' : '#fff' }} />
               </ListItemIcon>
               {!isLayoutSm && (
                 <ListItemText primary={'Withdraw'} className={classNames(current === INVEST_TAB.withdraw ? classes.check : classes.text)} />
               )}
             </ListItem>
+            <ListItem
+              key="Swap"
+              button
+              className={classNames(classes.item, current === INVEST_TAB.withdraw && classes.check)}
+              onClick={() => setCurrent(INVEST_TAB.swap)}
+            >
+              <ListItemIcon>
+                <SwapIcon style={{ color: current === INVEST_TAB.swap ? '#A68EFE' : '#fff' }} />
+              </ListItemIcon>
+              {!isLayoutSm && <ListItemText primary={'Swap'} className={classNames(current === INVEST_TAB.swap ? classes.check : classes.text)} />}
+            </ListItem>
             <ListItem key="Switch to USDi" button className={classNames(classes.item)} onClick={() => history.push('/mutils')}>
               <ListItemIcon>
-                <SwapHorizIcon style={{ color: '#fff' }} />
+                <SwitchIcon style={{ color: '#fff' }} />
               </ListItemIcon>
               {!isLayoutSm && <ListItemText primary={'Switch to USDi'} className={classNames(classes.text)} />}
             </ListItem>
