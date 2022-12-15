407fc91bdaa60774e73330212756b19deae985e0
韩旭gogogo <js_bin@163.com>
2022-12-15 10:51:17 +0800

    feature: add function for contract errors

--- src/helpers/error-handler.js
+++ src/helpers/error-handler.js
@@ -51,6 +51,33 @@ export const isRp = (errorMsg = '') => {
 }
 
 /**
+ * redeemFeeBps invalid
+ * @param {*} errorMsg
+ * @returns
+ */
+export const isRi = (errorMsg = '') => {
+  return errorMsg.endsWith("'RI'")
+}
+
+/**
+ * amount invalid
+ * @param {*} errorMsg
+ * @returns
+ */
+export const isAi = (errorMsg = '') => {
+  return errorMsg.endsWith("'AI'")
+}
+
+/**
+ * received less than minimum
+ * @param {*} errorMsg
+ * @returns
+ */
+export const isRLTM = (errorMsg = '') => {
+  return errorMsg.endsWith("'RLTM'")
+}
+
+/**
  * exchange error slippage not enough
  * @param {*} errorMsg
  * @returns
