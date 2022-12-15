df3226b38c779a27139e912e67e64b6c5af45031
韩旭gogogo <js_bin@163.com>
2022-12-15 10:50:15 +0800

    chore: abi upgrade

--- src/constants/abi.js
+++ src/constants/abi.js
@@ -5,6 +5,9 @@ export const ETHI_v1_5_10 = 'ethi-v1.5.10'
 export const USDI_V1_6_0 = 'usdi-v1.6.0'
 export const ETHI_V1_6_0 = 'ethi-v1.6.0'
 
+export const USDI_v1_6_9 = 'usdi-v1.6.9'
+export const ETHI_v1_6_9 = 'ethi-v1.6.9'
+
 export const COMMON = 'common'
 
 // abi rely on
@@ -14,5 +17,7 @@ export const ABI_SEQUENCE = {
   [ETHI]: [COMMON, ETHI],
   [ETHI_v1_5_10]: [COMMON, ETHI, ETHI_v1_5_10],
   [USDI_V1_6_0]: [COMMON, BETA_V1_5_9, USDI_V1_6_0],
-  [ETHI_V1_6_0]: [COMMON, ETHI, ETHI_V1_6_0]
+  [ETHI_V1_6_0]: [COMMON, ETHI, ETHI_V1_6_0],
+  [USDI_v1_6_9]: [COMMON, BETA_V1_5_9, USDI_V1_6_0, USDI_v1_6_9],
+  [ETHI_v1_6_9]: [COMMON, ETHI, ETHI_V1_6_0, ETHI_v1_6_9]
 }
