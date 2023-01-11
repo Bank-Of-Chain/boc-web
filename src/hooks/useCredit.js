4bb5ac9b37ea5cfaf6001100a26cc1c375e490d6
韩旭gogogo <js_bin@163.com>
2023-01-11 16:49:57 +0800

    feature: function logics add for CreditFacade

--- src/hooks/useCredit.js
+++ src/hooks/useCredit.js
@@ -1,14 +1,21 @@
 /* eslint-disable */
 import { useState } from 'react'
 
+// === Hooks === //
+import useUserAddress from '@/hooks/useUserAddress'
+
 // === Utils === //
 import * as ethers from 'ethers'
+import { useEffect } from 'react'
+import { isEmpty } from 'lodash'
+import { useCallback } from 'react'
 
 const { BigNumber } = ethers
 
 const useCredit = (CREDIT_POOL_ADDRESS, CREDIT_POOL_ABI, userProvider) => {
   const [balance, setBalance] = useState(BigNumber.from(0))
-  const [creditAddress, setCreditAddress] = useState('11')
+  const [creditAddress, setCreditAddress] = useState('')
+  const [hasOpenedCreditAccount, setHasOpenedCreditAccount] = useState(undefined)
   const [mortgageAmount, setMortgageAmount] = useState(BigNumber.from(0))
 
   const [debtAmount, setDebtAmount] = useState(BigNumber.from(0))
@@ -19,25 +26,82 @@ const useCredit = (CREDIT_POOL_ADDRESS, CREDIT_POOL_ABI, userProvider) => {
   const [vaultApy, setVaultApy] = useState(0)
   const [personalApy, setPersonalApy] = useState(0)
 
-  const connect = () => {
-    //TODO:
+  const address = useUserAddress(userProvider)
+
+  /**
+   *
+   * @param {*} borrowedAmount
+   * @param {*} leverageFactor
+   * @returns
+   */
+  const openCreditAccount = (borrowedAmount, leverageFactor) => {
+    const creditPoolContract = new ethers.Contract(CREDIT_POOL_ADDRESS, CREDIT_POOL_ABI, userProvider)
+    const signer = userProvider.getSigner()
+    return creditPoolContract.connect(signer).openCreditAccount(borrowedAmount, leverageFactor)
   }
 
-  const unConnect = () => {
-    //TODO:
+  /**
+   *
+   * @returns
+   */
+  const closeCreditAccount = () => {
+    const creditPoolContract = new ethers.Contract(CREDIT_POOL_ADDRESS, CREDIT_POOL_ABI, userProvider)
+    const signer = userProvider.getSigner()
+    return creditPoolContract.connect(signer).closeCreditAccount()
   }
 
-  const deposit = value => {
-    //TODO:
+  /**
+   *
+   * @param {*} value
+   */
+  const increaseDebt = value => {
+    const creditPoolContract = new ethers.Contract(CREDIT_POOL_ADDRESS, CREDIT_POOL_ABI, userProvider)
+    const signer = userProvider.getSigner()
+    creditPoolContract.connect(signer).increaseDebt(address)
   }
 
-  const withdraw = value => {
-    //TODO:
+  /**
+   *
+   * @param {*} value
+   */
+  const decreaseDebt = value => {
+    const creditPoolContract = new ethers.Contract(CREDIT_POOL_ADDRESS, CREDIT_POOL_ABI, userProvider)
+    const signer = userProvider.getSigner()
+    creditPoolContract.connect(signer).decreaseDebt(address)
   }
 
+  /**
+   *
+   */
+  const withdrawFromVault = () => {}
+
+  /**
+   *
+   */
+  const addCollateral = () => {}
+
+  /**
+   *
+   * @param {*} value
+   * @returns
+   */
   const leverRatioUpdate = value => {
     return 0
   }
+
+  /**
+   *
+   */
+  const queryCreditAddress = useCallback(() => {
+    if (isEmpty(CREDIT_POOL_ADDRESS) || isEmpty(CREDIT_POOL_ABI) || isEmpty(address) || isEmpty(userProvider)) return
+    const creditPoolContract = new ethers.Contract(CREDIT_POOL_ADDRESS, CREDIT_POOL_ABI, userProvider)
+    creditPoolContract
+      .hasOpenedCreditAccount(address)
+      .then(setHasOpenedCreditAccount)
+      .catch(() => setHasOpenedCreditAccount(false))
+  }, [CREDIT_POOL_ADDRESS, CREDIT_POOL_ABI, address, userProvider])
+
+  useEffect(queryCreditAddress, [queryCreditAddress])
 
   return {
     balance,
@@ -49,11 +113,14 @@ const useCredit = (CREDIT_POOL_ADDRESS, CREDIT_POOL_ABI, userProvider) => {
     borrowInterest,
     vaultApy,
     personalApy,
+    hasOpenedCreditAccount,
     // actions
-    connect,
-    unConnect,
-    deposit,
-    withdraw,
+    openCreditAccount,
+    closeCreditAccount,
+    increaseDebt,
+    decreaseDebt,
+    addCollateral,
+    withdrawFromVault,
     leverRatioUpdate
   }
 }
