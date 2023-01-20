0310c762f1cda4192fdc2a61113b58c5597291ce
韩旭gogogo <js_bin@163.com>
2023-01-20 15:18:21 +0800

    feature: abis add for resolver

--- src/hooks/useAbiResolver.js
+++ src/hooks/useAbiResolver.js
@@ -22,12 +22,28 @@ function abiLoader(version, file) {
 function useAbiResolver(abiPrefix) {
   if (isEmpty(abiPrefix)) return {}
 
-  let VAULT_BUFFER_ABI, VAULT_ABI, EXCHANGE_ADAPTER_ABI, PRICE_ORCALE_ABI, EXCHANGE_AGGREGATOR_ABI, POOL_SERVICE_ABI, CREDIT_POOL_ABI
+  let VAULT_BUFFER_ABI,
+    VAULT_ABI,
+    EXCHANGE_ADAPTER_ABI,
+    PRICE_ORCALE_ABI,
+    EXCHANGE_AGGREGATOR_ABI,
+    POOL_SERVICE_ABI,
+    CREDIT_FACADE_ABI,
+    CREDIT_ADDRESS_ABI,
+    STAKING_POOL_ABI,
+    CREDIT_MANAGER_ABI,
+    DIESEL_TOKEN_ABI
 
   try {
     VAULT_ABI = abiLoader(abiPrefix, 'vault-abi.json')
   } catch (error) {
     VAULT_ABI = []
+  }
+
+  try {
+    CREDIT_ADDRESS_ABI = abiLoader(abiPrefix, 'credit-account-abi.json')
+  } catch (error) {
+    CREDIT_ADDRESS_ABI = []
   }
 
   try {
@@ -37,9 +53,27 @@ function useAbiResolver(abiPrefix) {
   }
 
   try {
-    CREDIT_POOL_ABI = abiLoader(abiPrefix, 'credit-pool-abi.json')
+    STAKING_POOL_ABI = abiLoader(abiPrefix, 'staking-pool-abi.json')
   } catch (error) {
-    CREDIT_POOL_ABI = []
+    STAKING_POOL_ABI = []
+  }
+
+  try {
+    DIESEL_TOKEN_ABI = abiLoader(abiPrefix, 'diesel-token-abi.json')
+  } catch (error) {
+    DIESEL_TOKEN_ABI = []
+  }
+
+  try {
+    CREDIT_FACADE_ABI = abiLoader(abiPrefix, 'credit-facade-abi.json')
+  } catch (error) {
+    CREDIT_FACADE_ABI = []
+  }
+
+  try {
+    CREDIT_MANAGER_ABI = abiLoader(abiPrefix, 'credit-manager-abi.json')
+  } catch (error) {
+    CREDIT_MANAGER_ABI = []
   }
 
   try {
@@ -68,10 +102,14 @@ function useAbiResolver(abiPrefix) {
 
   return {
     VAULT_ABI,
-    CREDIT_POOL_ABI,
+    CREDIT_FACADE_ABI,
     POOL_SERVICE_ABI,
     VAULT_BUFFER_ABI,
     PRICE_ORCALE_ABI,
+    STAKING_POOL_ABI,
+    DIESEL_TOKEN_ABI,
+    CREDIT_MANAGER_ABI,
+    CREDIT_ADDRESS_ABI,
     EXCHANGE_ADAPTER_ABI,
     EXCHANGE_AGGREGATOR_ABI
   }
