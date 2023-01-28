377da3f44097571f6582c99f7a4347003820affa
韩旭gogogo <js_bin@163.com>
2023-01-28 11:27:44 +0800

    feature: base on erc20 hook and add Transfer event

--- src/hooks/useDieselToken.js
+++ src/hooks/useDieselToken.js
@@ -2,6 +2,7 @@ import { useState, useCallback, useEffect } from 'react'
 
 // === Hooks === //
 import useUserAddress from '@/hooks/useUserAddress'
+import useErc20Token from '@/hooks/useErc20Token'
 
 // === Utils === //
 import * as ethers from 'ethers'
@@ -10,6 +11,7 @@ import isEmpty from 'lodash/isEmpty'
 const { BigNumber } = ethers
 
 const useDieselToken = (DIESEL_TOKEN_ADDRESS, DIESEL_TOKEN_ABI, userProvider) => {
+  const erc20Data = useErc20Token(DIESEL_TOKEN_ADDRESS, userProvider)
   const [rewardAmounts, setRewardAmounts] = useState(BigNumber.from(0))
 
   const address = useUserAddress(userProvider)
@@ -37,11 +39,38 @@ const useDieselToken = (DIESEL_TOKEN_ADDRESS, DIESEL_TOKEN_ABI, userProvider) =>
     return creditManagerContract.earned(address).then(setRewardAmounts)
   }, [DIESEL_TOKEN_ADDRESS, DIESEL_TOKEN_ABI, address, userProvider])
 
+  /**
+   *
+   */
+  const handleTransfer = useCallback(
+    (fromAccount, toAccount) => {
+      if (fromAccount === address || toAccount === address) {
+        erc20Data.queryBalance()
+      }
+    },
+    [erc20Data.queryBalance, address]
+  )
+
+  useEffect(() => {
+    const listener = () => {
+      if (isEmpty(DIESEL_TOKEN_ADDRESS) || isEmpty(DIESEL_TOKEN_ABI) || isEmpty(userProvider) || isEmpty(address)) return
+      const vaultContract = new ethers.Contract(DIESEL_TOKEN_ADDRESS, DIESEL_TOKEN_ABI, userProvider)
+      if (!isEmpty(address)) {
+        vaultContract.on('Transfer', handleTransfer)
+        return () => {
+          vaultContract.off('Transfer', handleTransfer)
+        }
+      }
+    }
+    return listener()
+  }, [address, DIESEL_TOKEN_ADDRESS, DIESEL_TOKEN_ABI, userProvider, handleTransfer])
+
   useEffect(() => {
     getEarned()
   }, [getEarned])
 
   return {
+    ...erc20Data,
     rewardAmounts,
     // actions
     getReward
