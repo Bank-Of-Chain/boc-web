2d7486dc70e24122dc02b10c3f64b1dd9aeadd21
韩旭gogogo <js_bin@163.com>
2023-01-28 15:00:49 +0800

    feature: add event listener in hooks

--- src/hooks/useCreditFacade.js
+++ src/hooks/useCreditFacade.js
@@ -17,8 +17,8 @@ const useCreditFacade = (CREDIT_FACADE_ADDRESS, CREDIT_FACADE_ABI, userProvider)
   const [creditManagerAddress, setCreditManagerAddress] = useState('')
   const [hasOpenedCreditAccount, setHasOpenedCreditAccount] = useState(undefined)
   const [borrowInterest, setBorrowInterest] = useState(0)
-  const [vaultApy, setVaultApy] = useState(600)
-  const [personalApy, setPersonalApy] = useState(0)
+  const [vaultApy, setVaultApy] = useState(0)
+  const [personalApy, setPersonalApy] = useState(600)
 
   // search if current user has created CreditAccount address
   const [isCreditAddressLoading, setIsCreditAddressLoading] = useState()
@@ -35,10 +35,7 @@ const useCreditFacade = (CREDIT_FACADE_ADDRESS, CREDIT_FACADE_ABI, userProvider)
     (borrowedAmount, leverageFactor) => {
       const creditFacadeContract = new ethers.Contract(CREDIT_FACADE_ADDRESS, CREDIT_FACADE_ABI, userProvider)
       const signer = userProvider.getSigner()
-      return creditFacadeContract
-        .connect(signer)
-        .openCreditAccount(borrowedAmount, leverageFactor)
-        .finally(() => queryCreditAddress())
+      return creditFacadeContract.connect(signer).openCreditAccount(borrowedAmount, leverageFactor)
     },
     [CREDIT_FACADE_ADDRESS, CREDIT_FACADE_ABI, userProvider]
   )
@@ -165,6 +162,8 @@ const useCreditFacade = (CREDIT_FACADE_ADDRESS, CREDIT_FACADE_ABI, userProvider)
             .getCreditAccount(address)
             .then(setCreditAddress)
             .catch(() => setCreditAddress(''))
+        } else {
+          setCreditAddress('')
         }
       })
       .catch(() => setHasOpenedCreditAccount(false))
@@ -187,6 +186,61 @@ const useCreditFacade = (CREDIT_FACADE_ADDRESS, CREDIT_FACADE_ABI, userProvider)
     },
     [CREDIT_FACADE_ADDRESS, CREDIT_FACADE_ABI, address, userProvider]
   )
+
+  /**
+   *
+   */
+  const handleOpenCreditAccount = useCallback(
+    sender => {
+      if (sender === address) {
+        queryCreditAddress()
+      }
+    },
+    [queryCreditAddress, address]
+  )
+
+  /**
+   *
+   */
+  const handleAddCollateral = useCallback(
+    sender => {
+      if (sender === address) {
+        //TODO: add logics
+      }
+    },
+    [queryCreditAddress, address]
+  )
+
+  /**
+   *
+   */
+  const handleRedeemCollateral = useCallback(
+    sender => {
+      if (sender === address) {
+        //TODO: add logics
+      }
+    },
+    [queryCreditAddress, address]
+  )
+
+  useEffect(() => {
+    const listener = () => {
+      if (isEmpty(CREDIT_FACADE_ADDRESS) || isEmpty(CREDIT_FACADE_ABI) || isEmpty(userProvider) || isEmpty(address)) return
+      const vaultContract = new ethers.Contract(CREDIT_FACADE_ADDRESS, CREDIT_FACADE_ABI, userProvider)
+      vaultContract.on('OpenCreditAccount', handleOpenCreditAccount)
+      vaultContract.on('CloseCreditAccount', handleOpenCreditAccount)
+      vaultContract.on('AddCollateral', handleAddCollateral)
+      vaultContract.on('RedeemCollateral', handleRedeemCollateral)
+
+      return () => {
+        vaultContract.off('OpenCreditAccount', handleOpenCreditAccount)
+        vaultContract.off('CloseCreditAccount', handleOpenCreditAccount)
+        vaultContract.off('AddCollateral', handleAddCollateral)
+        vaultContract.off('RedeemCollateral', handleRedeemCollateral)
+      }
+    }
+    return listener()
+  }, [address, CREDIT_FACADE_ADDRESS, CREDIT_FACADE_ABI, userProvider, handleOpenCreditAccount])
 
   return {
     creditAddress,
