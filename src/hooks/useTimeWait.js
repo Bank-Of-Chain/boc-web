3cd1f72f6a952e8fd33bcac9a06336e89eb44a08
韩旭gogogo <js_bin@163.com>
2023-02-02 13:57:19 +0800

    feature: add hooks for time waiting

---
+++ src/hooks/useTimeWait.js
@@ -0,0 +1,26 @@
+import { useEffect, useState, useReducer } from 'react'
+
+// === Utils === //
+import moment from 'moment'
+
+const useTimeWait = (duration = 1000) => {
+  const [initMoment, setInitMoment] = useState()
+  const [, forceUpdate] = useReducer(x => x + 1, 0)
+
+  useEffect(() => {
+    setInitMoment(moment())
+  }, [])
+
+  useEffect(() => {
+    forceUpdate()
+    const timer = setInterval(forceUpdate, duration)
+    return () => clearInterval(timer)
+  }, [forceUpdate])
+
+  return {
+    initMoment,
+    fromNow: moment.utc(moment().diff(initMoment)).format('HH:mm:ss')
+  }
+}
+
+export default useTimeWait
