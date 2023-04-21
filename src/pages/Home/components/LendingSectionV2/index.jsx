9016b346ba8f58929ef5eae591e94368046b56f5
韩旭gogogo <js_bin@163.com>
2023-04-21 17:36:19 +0800

    feature: notices add in home page

--- src/pages/Home/components/LendingSectionV2/index.jsx
+++ src/pages/Home/components/LendingSectionV2/index.jsx
@@ -219,6 +219,27 @@ export default function LendingSectionV2() {
                 )}
               </GridContainer>
             </GridItem>
+            <GridItem md={12} xs={12} sm={12}>
+              <p style={{ color: '#ec4899', background: 'yellow', padding: '1rem', margin: '1rem' }}>
+                <svg
+                  style={{ verticalAlign: 'middle' }}
+                  viewBox="64 64 896 896"
+                  focusable="false"
+                  data-icon="sound"
+                  width="1em"
+                  height="1em"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path d="M625.9 115c-5.9 0-11.9 1.6-17.4 5.3L254 352H90c-8.8 0-16 7.2-16 16v288c0 8.8 7.2 16 16 16h164l354.5 231.7c5.5 3.6 11.6 5.3 17.4 5.3 16.7 0 32.1-13.3 32.1-32.1V147.1c0-18.8-15.4-32.1-32.1-32.1zM586 803L293.4 611.7l-18-11.7H146V424h129.4l17.9-11.7L586 221v582zm348-327H806c-8.8 0-16 7.2-16 16v40c0 8.8 7.2 16 16 16h128c8.8 0 16-7.2 16-16v-40c0-8.8-7.2-16-16-16zm-41.9 261.8l-110.3-63.7a15.9 15.9 0 00-21.7 5.9l-19.9 34.5c-4.4 7.6-1.8 17.4 5.8 21.8L856.3 800a15.9 15.9 0 0021.7-5.9l19.9-34.5c4.4-7.6 1.7-17.4-5.8-21.8zM760 344a15.9 15.9 0 0021.7 5.9L892 286.2c7.6-4.4 10.2-14.2 5.8-21.8L878 230a15.9 15.9 0 00-21.7-5.9L746 287.8a15.99 15.99 0 00-5.8 21.8L760 344z"></path>
+                </svg>
+                &nbsp; The BOC team is preparing for the BOC V2.0 release development and release, so the regular operation of the current online V1.0
+                release is suspended (including regular harvesting and warehousing). Therefore, it is expected that BoC APY will continue to decline
+                even to 0% in the near term. BOC V1.0 has invested in the strategy is still operating normally and the actual yield will be reflected
+                in the future when the strategy is reaped at an appropriate time. We&apos;re going to bring you a whole new version. Please contact us
+                if you have any questions or suggestions (contact@bankofchain.io)
+              </p>
+            </GridItem>
           </GridContainer>
         </div>
       </div>
