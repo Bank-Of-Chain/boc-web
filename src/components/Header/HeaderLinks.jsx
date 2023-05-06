24a5373c12edc844691a577a06148b00770219ba
韩旭gogogo <js_bin@163.com>
2023-05-06 16:14:23 +0800

    refactor: url update

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -115,7 +115,7 @@ export default function HeaderLinks(props) {
     <>
       <List className={classes.list}>
         <ListItem className={classes.listItem}>
-          <Button color="colorful-text" className={classes.m4} target="_blank" href={'https://v2.bankofchain.io'} disableRipple={true}>
+          <Button color="colorful-text" className={classes.m4} target="_blank" href={'https://bankofchain.io'} disableRipple={true}>
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
               <path
                 fill="#eab308"
