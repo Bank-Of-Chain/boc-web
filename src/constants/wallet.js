f16a7538771aaeae401fc7ef829701e5e5e42e0e
韩旭gogogo <js_bin@163.com>
2023-06-28 17:50:24 +0800

    chore: omit WalletConnect item

--- src/constants/wallet.js
+++ src/constants/wallet.js
@@ -2,6 +2,7 @@ import WalletConnectProvider from '@walletconnect/web3-provider'
 
 // === Utils === //
 import map from 'lodash/map'
+import omit from 'lodash/omit'
 
 // === Constants === //
 import { RPC_URL } from '@/constants'
@@ -34,4 +35,4 @@ export const WALLETS = {
   }
 }
 
-export const WALLET_OPTIONS = map(WALLETS, wallet => wallet.info)
+export const WALLET_OPTIONS = map(omit(WALLETS, 'WalletConnect'), wallet => wallet.info)
